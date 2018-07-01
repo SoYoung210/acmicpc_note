5bf2b38aecf9dc8d3264655d8955fb0fa02b6404
SoYoung210 <ethdud1@gamil.com>
2018-07-02 00:13:25 +0900

    단순 큐 구현 문제.... 다른문제를 더...

--- DS/SY_kkao+boj/KCF2.java
+++ DS/SY_kkao+boj/KCF2.java
@@ -1,32 +1,59 @@
 import java.util.*;
 
 public class KCF2 {
-	public static int solution(int cacheSize, Stirng[] cities) {
+	public static int solution(int cacheSize, String[] cities) {
 		//소요시간, 즉 정답.
 		int time = 0;
 		if(cacheSize==0) {
 			return cities.length *5;
 		}
-		String[] cache = new String[cacheSize];
+		Queue<String> c = new LinkedList<String>();
 		int index = 0;
-		int cache_index = 0;
 		int citi_len = cities.length;
 		boolean isHit = false;
-		//캐쉬부터 채우자.
-		for(index=0; index<=cacheSize; index++) {
-			time += 5;
-			cache[index] = cities[index];
-		}
-		while(index < citi_len) {
-			for(cache_index = 0; cache_index<cacheSize; cache_index++) {
-				
-			}
+		int cache_index = 0;
 
+		//캐쉬부터 채우자.
+		for(index=0; index<cacheSize; index++) {
+			time += 5;
+			c.add(cities[index]);
+		}
+
+		while(index < citi_len) {
+			isHit = false;
+
+			for(String tmp : c) {
+				//Cache tmp = c.peek();
+				if(cities[index].equals(tmp)) {
+					//지우고 다시넣기.x
+					//System.out.println(tmp.city);
+					c.remove(tmp);
+					c.add(tmp);
+					isHit = true;
+					break;
+				}
+			}
+			if(isHit) {
+				time++;
+			}else {
+				time += 5;
+				c.poll();
+				c.add(cities[index]);
+			}
 			index++;
 		}
+		return time;
 	}
+
 	public static void main (String[] args) throws Exception {
-		
-		
+		String[] city1 = {"jeju" , "pangyo" , "soeul", "newyork", "la","jeju", "pangyo" ,"soeul" ,"newyork", "la"};
+		String[] city2 = {"jeju" , "pangyo" , "soeul", "jeju", "pangyo","soeul", "jeju" ,"pangyo" ,"soeul"};
+		String[] city3 = {"jeju" , "pangyo" , "newyork", "newyork"};
+		System.out.println(solution(3, city1));
+		System.out.println("============");
+		System.out.println(solution(3, city2));
+		System.out.println(solution(2, city3));
 	}
 }	
+
+
