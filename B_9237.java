119dfa1c67671707a67885fa94250cce4660c09c
SoYoung210 <ethdud1@gamil.com>
2018-07-10 09:06:37 +0900

    # Priority Queue Success!

--- B_9237.java
+++ B_9237.java
@@ -6,21 +6,19 @@ public class B_9237 {
 	public static void main (String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int count = Integer.parseInt(br.readLine());
-		ArrayList<Integer> tree = new ArrayList<>(count);
+		PriorityQueue<Integer> tree = new PriorityQueue<>(count, Collections.reverseOrder());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int i;
-		
+		//int tmp;
 		for(i=0;i<count;i++) {
-			tree.add(Integer.parseInt(st.nextToken()));
+			tree.offer(Integer.parseInt(st.nextToken()));
 		}
-		Collections.sort(tree);
 		int max = 0;
-		
-		i = count;
-		for(Integer tmp : tree) {
-			max = (max < tmp+i) ? tmp+i : max;
-			i--;
+		int tmp;
+		for(i=1;i<=count;i++) {
+			tmp = tree.poll()+i;
+			max = (max < tmp) ? tmp : max;
 		}
 		System.out.println(max+1);
 	}
