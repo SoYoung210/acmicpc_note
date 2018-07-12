8a1fdc46ef5c01c719f75e21d9c85264e30f7fb8
SoYoung210 <ethdud1@gamil.com>
2018-07-13 01:19:40 +0900

    # 2504 method 정리, compile error fix.

--- B_2504.java
+++ B_2504.java
@@ -4,8 +4,9 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 //(()[[]])([]) answer : 28
 public class B_2504 {
+	static Stack<String> s = new Stack<>();
 	public static void main(String[] args) {
-		Stack<Character> s = new Stack<>();
+		
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			char[] chArr = br.readLine().toCharArray();
 			int chLeng = chArr.length;
@@ -14,15 +15,22 @@ public class B_2504 {
 			int answer = 0;
 
 			int pairIndex;
-			System.out.println("chLeng : "+chLeng);
+
 			for(i=0; i < chLeng; i++) {
 				//stackOp call
+				if(chArr[i] == ')') {
+					stackOp(2,')');
+				}else if(chArr[i] == ']') {
+					stackOp(2,')');
+				}else {
+					s.push(Character.toString(chArr[i]));
+				}
 			}
 
 			while(!s.isEmpty()) {
 				System.out.println("s.size() : "+s.size());
-				if(Character.isDigit(s.peek())) {
-					answer += Character.getNumericValue(s.pop());
+				if(isInteger(s.peek())) {
+					answer += Integer.parseInt(s.pop());
 				}else {
 					System.out.println("0"); return;
 				}
@@ -34,23 +42,50 @@ public class B_2504 {
 	}
 	public static void stackOp(int op,char bracket) {
 		int sum = 0;
-		char tmp;
-		char tmp2;
-		char second;
-		switch(bracket) {
-			case ')': {
+		String tmp;
+		String tmp2;
+		String second;
 
-				break;
-			}
-			case ']': {
+		tmp = s.pop();
+		if(isInteger(tmp)) {
+			sum += Integer.parseInt(tmp);
+			try {
+				second = s.peek();
+				System.out.println("second : "+second);
+				if(isInteger(second)) {
+					tmp2 = s.peek();
+					while(isInteger(tmp2)) {
+						s.pop();
+						sum += Integer.parseInt(tmp2);
+						tmp2 = s.peek();
+					}
+					s.push(Integer.toString(sum*op));
+				}else {
+					s.pop();
+					s.push(Integer.toString(Integer.parseInt(tmp)*op));
+				}
 
-				break;
+			}catch(Exception e) {
+				System.out.println("0");
 			}
-			default: {
-
-				break;
+		}else {
+			if(bracket == '(') {
+				s.push("2");
+			}else {
+				s.push("3");
 			}
+			//s.push( (bracket=='(') ? '2' : '3' );
 		}
-
+	}
+	public static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    } catch(NullPointerException e) {
+	        return false;
+	    }
+	    // only got here if we didn't return false
+	    return true;
 	}
 }
