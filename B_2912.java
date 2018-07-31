06bb21cad6f8beb98cef0b12f2c021f19c95b78d
SoYoung210 <ethdud1@gamil.com>
2018-07-31 10:02:56 +0900

    2912 번... 분명 더 최적의 방법이 있을 듯.

--- B_2912.java
+++ B_2912.java
@@ -23,41 +23,28 @@ public class B_2912 {
 			int indexDwarf;
 			//모자 종류 배열
 			int[] dwarf_hat = new int[c+1];
-
-			boolean half_check = false;
-			int half_num = 0;
-			int retIndex = 0;
+			boolean isPretty ;
 			for(i=0; i<m; i++) {
+				isPretty = false;
 				tmp = br.readLine().split(" ");
 				//배열은 0부터 여기 숫자는 1부터 시작.
 				int startDwarf = Integer.parseInt(tmp[0]);
 				int endDwarf = Integer.parseInt(tmp[1]);
+				int half = (endDwarf - startDwarf +1)/2;
 
 				for(int j=startDwarf; j<=endDwarf;j++) {
 					indexDwarf = dwarf[j];
-					dwarf_hat[indexDwarf]++;					
+					dwarf_hat[indexDwarf]++;
+
+					if(dwarf_hat[indexDwarf] > (endDwarf - startDwarf +1)/2 ) {
+						System.out.println("yes "+indexDwarf);
+						isPretty = true;
+						break;
+					}		
 				}
 
-				//Arrays.binarySearch와 성능 비교해보기. 뭘비교해 압살이지 ;
-				//짝 홀 
-				if( (endDwarf - startDwarf +1)%2 ==0 ){
-					half_num = ((endDwarf - startDwarf +1)/2);
-				}else {
-					half_num = ((endDwarf - startDwarf +1)/2) + 1;
-				}
-				System.out.println("half_num "+half_num);
+				if(!isPretty) System.out.println("no");
 
-
-				//java8을 쓰기는 힘들듯 ;
-				/*
-				half_check = IntStream.of(dwarf_hat).anyMatch(x -> x >= (endDwarf - startDwarf +1));
-				if(half_check) {
-					//자바 8을 쓰려는 비효율의 극치
-					Arrays.sort(dwarf_hat);
-					System.out.println("yes "+dwarf_hat[dwarf_hat.length-1]);
-				}else {
-					System.out.println("no");
-				}*/
 				Arrays.fill(dwarf_hat,0);
 			}
 		}catch(Exception e) {
