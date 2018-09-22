fe9f889f9ce8299c543316e23203479ab905b517
SoYoung210 <ethdud1@gmail.com>
2018-09-22 10:42:18 +0900

    수정

--- L_63.java
+++ L_63.java
@@ -8,11 +8,19 @@ public class L_63 {
 	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
 	    int m = obstacleGrid.length;
 	    int n = obstacleGrid[0].length;
-	 	int i;
+	 	int i,j;
 	    if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1) 
 	        return 0;
 	    int[][] dp = new int[m][n];
     	dp[0][0]=1;
+
+    	for(i=1; i<m; i++) {
+    		if(obstacleGrid[i][0] == 1) {
+    			dp[i][0] = 0;
+    		}else {
+    			dp[i][0] = dp[i-1][0];
+    		}
+    	}
 
     	for(i=1; i<n; i++) {
     		if(obstacleGrid[0][i] == 1) {
@@ -21,13 +29,15 @@ public class L_63 {
     			dp[0][i] = dp[0][i-1];
     		}
     	}
-
+    	for(i=1; i<m; i++) {
+    		for(j=1; j<n; j++) {
+    			if(obstacleGrid[i][j]==1){
+                	dp[i][j]=0;
+	            }else{
+	                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+	            }
+    		}
+    	}
+    	return dp[m-1][n-1];  
     }
-	public static void dfs(int here,int[][] obstacleGrid){
-		visited[here] = true;
-		
-		for(int i=0; i<obstacleGrid.length; i++){
-			if(!visited[i] && obstacleGrid[here][i]==0) dfs(i, obstacleGrid);
-		}
-	}
 }
